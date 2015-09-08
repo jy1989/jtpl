14752ca5e74e14eb2e04721b273f815c58895a4f
chenjianyi <chen@chentekiiMac.local>
2015-09-08 11:58:59 +0800

    update

--- jtpl.js
+++ jtpl.js
@@ -3,50 +3,44 @@
  * ===========================================================
  * https://github.com/jy1989/jtpl
  * ========================================================== */
-function jtpl() {
-	//var result=tpl;
-	var argLen = arguments.length;
-	if (argLen < 3) {
-		return 'parameter < 3';
-	}
-	var tpl = arguments[0] || '{v1}';
-	var type = arguments[1] || 'string';
-	//
 
-	if (type == 'object') {
-		var i;
-		var data = arguments[2];
+; (function(jy, undefined) {
+
+	jy.jtpl = function() {
+		var argLen = arguments.length;
+		var tpl = arguments[0] || '{v1}';
+		var type = typeof arguments[1];
+		if (type === 'object') {
+			var i;
+			var data = arguments[1];
+			for (i in data) {
+				if (typeof data[i] === 'object') {
+					tpl = replaceDeep(tpl, data[i], i);
+				} else {
+					tpl = tpl.replace(eval('/{' + i + '}/g'), data[i]);
+				}
+			}
+
+		} else if (type === 'string' || type === 'number' || type === 'boolean') {
+			for (var i = 1; i < arguments.length; i++) {
+				tpl = tpl.replace(eval('/{v' + (i) + '}/g'), arguments[i] + '');
+			}
+
+		}
+		return tpl;
+	}
+
+	function replaceDeep(tpl, data, fatherName) {
 		for (i in data) {
-			console.log(typeof data[i], i, data[i]);
+			var prefix = fatherName + '.';
 			if (typeof data[i] === 'object') {
-				tpl = replaceDeep(tpl, data[i], i);
+				tpl = replaceDeep(tpl, data[i], prefix + i);
 			} else {
-				tpl = tpl.replace(eval('/{' + i + '}/g'), data[i]);
+
+				tpl = tpl.replace(eval('/{' + prefix + i + '}/g'), data[i]);
 			}
 		}
-
-	} else if (type == 'string') {
-
-		for (var i = 2; i < arguments.length; i++) {
-			tpl = tpl.replace(eval('/{v' + (i - 1) + '}/g'), arguments[i]);
-		}
+		return tpl;
 
 	}
-	return tpl;
-}
-
-function replaceDeep(tpl, data, fatherName) {
-	for (i in data) {
-		var prefix = fatherName + '.';
-		if (typeof data[i] === 'object') {
-			//console.log(prefix+'.'+i);
-			tpl = replaceDeep(tpl, data[i], prefix + i);
-		} else {
-
-			tpl = tpl.replace(eval('/{' + prefix + i + '}/g'), data[i]);
-		}
-	}
-
-	return tpl;
-
-}
+} (window.jy = window.jy || {}));

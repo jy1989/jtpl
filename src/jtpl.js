ba5b46996b0ad347556e2c1ca0af9d403db6e4b1
chenjianyi <chen@chentekiiMac.local>
2015-09-09 17:15:48 +0800

    grunt build

--- src/jtpl.js
+++ src/jtpl.js
@@ -1,43 +1,46 @@
-/* ===========================================================
- * jtpl.js
- * ===========================================================
- * https://github.com/jy1989/jtpl
- * ========================================================== */
-;
-(function(jy, undefined) {
+;(function(jy, undefined) {
 
     jy.jtpl = function() {
         var argLen = arguments.length;
         var tpl = arguments[0] || '{v1}';
         var type = typeof arguments[1];
+        var i;
+        var re;
         if (type === 'object') {
-            var i;
+           
             var data = arguments[1];
             for (i in data) {
                 if (typeof data[i] === 'object') {
                     tpl = replaceDeep(tpl, data[i], i);
                 } else {
-                    tpl = tpl.replace(eval('/{' + i + '}/g'), data[i]);
+                	re= new RegExp('{'+i+'}', "gi");
+                    //tpl = tpl.replace(eval('/{' + i + '}/g'), data[i]);
+                    tpl = tpl.replace(re, data[i]);
                 }
             }
 
         } else if (type === 'string' || type === 'number' || type === 'boolean') {
-            for (var i = 1; i < arguments.length; i++) {
-                tpl = tpl.replace(eval('/{v' + (i) + '}/g'), arguments[i] + '');
+            for ( i = 1; i < arguments.length; i++) {
+            	re= new RegExp('{v'+i+'}', "gi");
+                //tpl = tpl.replace(eval('/{v' + (i) + '}/g'), arguments[i] + '');
+                tpl = tpl.replace(re, arguments[i] + '');
             }
 
         }
         return tpl;
-    }
+    };
 
     function replaceDeep(tpl, data, fatherName) {
+    	var i;
+    	var reg;
         for (i in data) {
             var prefix = fatherName + '.';
             if (typeof data[i] === 'object') {
                 tpl = replaceDeep(tpl, data[i], prefix + i);
             } else {
-
-                tpl = tpl.replace(eval('/{' + prefix + i + '}/g'), data[i]);
+				reg= new RegExp('{'+prefix + i+'}', "gi");
+                //tpl = tpl.replace(eval('/{' + prefix + i + '}/g'), data[i]);
+                tpl = tpl.replace(reg, data[i]);
             }
         }
         return tpl;
